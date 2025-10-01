3f2d76e6fad43ddf764e1cd229b75f0385c65f33
Jamie Boseley <jboseley@uoguelph.ca>
2025-10-01 11:56:14 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -8,14 +8,17 @@
 
 num = input("Please enter then number of terms: ")
 
+#Checks if input is a number and/or a positive number
 if not num.isdigit() or int(num) <= 0:
   print("Please enter a positive integer.")
+
+#If input is a positive integer, make it an int. Set starting digits.
 else:
   num = int(num)
   a, b = 0, 1
 
   print(f"Fibonacci sequence up to {num} terms:")
-
+  #Runs the loop num times printing the current number and calculating the next sequence.
   for i in range(num):
     print(a, end=" ")
     a, b = b, a + b
